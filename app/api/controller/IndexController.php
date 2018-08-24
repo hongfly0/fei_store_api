7d5f448eea87d0b09cf01c454f23fbce3990c5d3
Fable <278380854@qq.com>
2018-08-24 11:30:13 +0800

    api模塊提交

---
+++ app/api/controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 首页接口api
+ */
+namespace app\user\controller;
+
+use app\user\model\UserModel;
+use app\api\model\GoodsModel;
+
+class IndexController
+{
+
+    /*
+     * 首页商品列表api
+     */
+    public function index(){
+
+        
+
+    }
+}
