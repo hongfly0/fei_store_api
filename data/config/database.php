39d680a91bf6c4b34e837fc17c08654b0c02ab77
Fable <278380854@qq.com>
2018-09-09 18:11:56 +0800

    忽略文件提交

---
+++ data/config/database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '127.0.0.1',
+    // 数据库名
+    'database' => 'fei_store',
+    // 用户名
+    'username' => 'root',
+    // 密码
+    'password' => 'Feng2163906@',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8mb4',
+    // 数据库表前缀
+    'prefix'   => 'sky_',
+    "authcode" => '5aoRr5HIUDsVH8TLhg',
+    //#COOKIE_PREFIX#
+];
---
+++ data/config/route.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fable
+ * Date: 18/9/9
+ * Time: 下午6:10
+ */
+  return array ();
